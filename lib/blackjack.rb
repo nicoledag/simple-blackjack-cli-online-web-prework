6f115ccf4cce535943c1b12210cb597836145257
Nicole D <nnardella@gmail.com>
2018-09-22 21:47:55 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -63,13 +63,12 @@ end
 
 def runner
   welcome
-  deal_card = initial_round
-  hit?
-  display_card_total
-
-   until display_card_total > 21
-
-  end_game
+card_total = initial_round
+until card_total > 21
+ card_total = hit?(card_total)
+ display_card_total(card_total)
+ end
+end_game(card_total)
 
  end
 end
